00ed7565b4a2d351ad42e73d22e2bc5f5001a0e2
Iris Stream <ngodaisonn@gmail.com>
2021-04-13 15:16:35 +0700

    Update get students' scores source and first 4000 data links

--- crawler/get_scores.py
+++ crawler/get_scores.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 import subprocess
 
 def remove_html_tags(text):
@@ -14,18 +15,67 @@ def remove_t(text):
     return re.sub(clean, '', text)
 
 def remove_leading_space(text):
-    text = text.replace("   ","")
+    # text = text.replace("   ","")
     return text.replace("  ", " ")
 
-url = 'curl https://diemthi.vnexpress.net/diem-thi-nam-2018/detail/id/3522034/'
+def is_float(x):
+    try:
+        float(x)
+        return True
+    except:
+        return False
 
-s = subprocess.check_output('curl https://diemthi.vnexpress.net/diem-thi-nam-2018/detail/id/3522034/', shell=True)
+def get_student(url):
+    s = subprocess.check_output(f"curl {url}", shell=True)
+    s = remove_html_tags(str(s))
+    s = s.strip()
+    s = remove_n(s)
+    s = remove_t(s)
+    s = remove_leading_space(s)
+    return s.split(' ')
 
-s = remove_html_tags(str(s))
-s = s.strip()
-s = remove_n(s)
-s = remove_t(s)
-s = remove_leading_space(s)
-s = s.split(' ')
-for ss in s:
-    print(ss)
+fin = open('output1.txt','rt')
+lines = fin.readlines()
+fin.close()
+
+fout = open('out', 'w')
+df = pd.DataFrame(columns=['SBD', 'Toan', 'Ngu van', 'Ngoai ngu', 'Vat ly', 'Hoa hoc', 'Sinh hoc', 'Lich su', 'Dia ly', 'GDCD'])
+i = 0
+for line in lines:
+    line = line.strip()
+    if len(line) < 10:
+        continue
+    s = get_student(line)
+    s.append('To\\xc3\\xa1n:')
+    s.append('')
+    s.append('v\\xc4\\x83n:')
+    s.append('')
+    s.append('ng\\xe1\\xbb\\xaf:')
+    s.append('')
+    s.append('V\\xe1\\xba\\xadt')
+    s.append('')
+    s.append('')
+    s.append('H\\xc3\\xb3a')
+    s.append('')
+    s.append('')
+    s.append('Sinh')
+    s.append('')
+    s.append('')
+    s.append('L\\xe1\\xbb\\x8bch')
+    s.append('')
+    s.append('')
+    s.append('\\xc4\\x90\\xe1\\xbb\\x8ba')
+    s.append('')
+    s.append('')
+    s.append('d\\xc3\\xa2n:')
+    s.append('')
+
+    for p in s:
+        fout.write(f"{p}\n")
+    df.loc[i] = [s[s.index('danh:')+1][:8], s[s.index('To\\xc3\\xa1n:') + 1], s[s.index('v\\xc4\\x83n:') + 1], s[s.index('ng\\xe1\\xbb\\xaf:') + 1], s[s.index('V\\xe1\\xba\\xadt') + 2], s[s.index('H\\xc3\\xb3a') + 2], s[s.index('Sinh') + 2], s[s.index('L\\xe1\\xbb\\x8bch') + 2], s[s.index('\\xc4\\x90\\xe1\\xbb\\x8ba') + 2], s[s.index('d\\xc3\\xa2n:') + 1]]
+    i += 1
+
+df.set_index('SBD')
+fout.close()
+
+df.to_csv('data.csv')
--- crawler/get_student.py
+++ crawler/get_student.py
@@ -1,4 +1,6 @@
 from requests_html import HTMLSession
+import time
+import random
 
 session = HTMLSession()
 
@@ -7,7 +9,8 @@ f = open("output.txt", "a")
 for area_code in range(5, 66):
     area_code_text = str(area_code)
     area_code_text = area_code_text.zfill(2)
-    for student_id in range(1653,20000):
+    for student_id in range(2458,20000):
+        time.sleep(random.random())
         student_id_text = str(student_id)
         student_id_text = student_id_text.zfill(5)
         url = f"https://diemthi.vnexpress.net/diem-thi-nam-2018#area=2&college=0&q={area_code_text}0{student_id_text}"
